f4d2d5d700c2ba71c6b7295441c160e84a5025cd
PDF Operator <xf28id1@xf28id1-srv1.nsls2.bnl.local>
2022-07-09 14:58:25 -0400

    updated plans

--- startup/99-qs_agent_plans.py
+++ startup/99-qs_agent_plans.py
@@ -1,10 +1,18 @@
 
 def agent_sample_count(position: float, exposure: float, *, md=None):
     yield from bps.mv(Grid_X, position)
-    _md = dict(Grid_X=Grid_X.read())
+    _md = dict(
+        Grid_X=Grid_X.read(),
+        Grid_Y=Grid_Y.read(),
+        Grid_Z=Grid_Z.read(),
+        Det_1_X=Det_1_X.read(),
+        Det_1_Y=Det_1_Y.read(),
+        Det_1_Z=Det_1_Z.read(),
+        ring_current=ring_current.read(),
+        BStop1=BStop1.read(),
+    )
     _md.update(md or {})
     yield from simple_ct([pe1c], exposure, md=_md)
-
 
 @bpp.run_decorator(md={})
 def agent_driven_nap(delay: float, *, delay_kwarg: float = 0):
